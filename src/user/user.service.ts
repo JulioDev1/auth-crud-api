7b0bc89caecf510fad126042feffd523c7e302b7
Julio Cesar <juliocesargualberto617@gmail.com>
2024-12-07 12:24:46 -0300

    refactor: adjust cryptography to password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { UserDto } from './dto/user.dto';
 import { User } from './entities/user.entity';
@@ -11,17 +12,25 @@ export class UserService {
   ) {}
 
   async create(user: UserDto): Promise<User> {
-    const userExists = await this.userRepository.find({
+    const userExists = await this.userRepository.findOne({
       where: {
         email: user.email,
       },
     });
-
+    console.log(userExists);
     if (userExists) {
       throw new ConflictException('User already exists');
     }
 
-    const userCreated = await this.userRepository.create(user);
+    const newUser = {
+      ...user,
+      password: await bcrypt.hash(
+        user.password,
+        Number(process.env.SALT_PASSWORD),
+      ),
+    };
+
+    const userCreated = await this.userRepository.create(newUser);
 
     return await this.userRepository.save(userCreated);
   }
